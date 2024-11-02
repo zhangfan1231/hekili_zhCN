a5346f8a02b4cce8f89e7ab580683c28a2a73d4c
syrifgit <139663837+syrifgit@users.noreply.github.com>
2024-11-02 02:34:26 -0300

    accidental revert

--- TheWarWithin/RogueAssassination.lua
+++ TheWarWithin/RogueAssassination.lua
@@ -2110,9 +2110,9 @@ spec:RegisterAbilities( {
             local fanCP = buff.clear_the_witnesses.up and 2 or 1
 
             -- Predict crit gains
-            if talent.seal_fate.enabled and settings.fan_knives_critical_prediction ~= "do_not_predict" then
+            if talent.seal_fate.enabled and settings.fok_critical_cp_prediction ~= "do_not_predict" then
                 -- calculate the crit chance of Fan of Knives then estimate 
-                fanCP = fanCP + max(0, floor( true_active_enemies * ( 0.01 * ( crit_pct_current + ( talent.deadly_precision.enabled and 5 or 0 ) + ( talent.thrown_precision.enabled and 5 or 0 ) + ( buff.momentum_of_despair.up and 10 or 0 ) + ( buff.master_assassin_any.up and 20 or 0 ) ) ) ) - ( settings.fan_knives_critical_prediction == "predict_conservatively" and 1 or 0 ) )
+                fanCP = fanCP + max(0, floor( true_active_enemies * ( 0.01 * ( crit_pct_current + ( talent.deadly_precision.enabled and 5 or 0 ) + ( talent.thrown_precision.enabled and 5 or 0 ) + ( buff.momentum_of_despair.up and 10 or 0 ) + ( buff.master_assassin_any.up and 20 or 0 ) ) ) ) - ( settings.fok_critical_cp_prediction == "predict_conservatively" and 1 or 0 ) )
 
             end
 
@@ -2916,7 +2916,7 @@ spec:RegisterStateExpr( "envenom_at_5cp", function ()
     return settings.envenom_pool_5_points and 1 or 0
 end )--]]
 
-spec:RegisterSetting( "fan_knives_critical_prediction", "predict", {
+spec:RegisterSetting( "fok_critical_cp_prediction", "predict", {
     name = strformat( "%s Critical Combo Point Prediction", Hekili:GetSpellLinkWithTexture( 51723 ) ),  -- Fan of Knives
     desc = strformat( "%sThis setting controls how %s predicts critical strikes for combo point generation.%s " ..
                       "|n|n%sThis option only works if the %s talent is selected.%s\n\n" ..
