d23e2610f2d958c98f730c02082cf7e065b4499b
syrifgit <139663837+syrifgit@users.noreply.github.com>
2024-11-15 12:08:45 -0400

    Feral - Bloodtalons fix
    Swipe typo, wasn't counting for bloodtalons.

--- TheWarWithin/DruidFeral.lua
+++ TheWarWithin/DruidFeral.lua
@@ -2700,7 +2700,7 @@ spec:RegisterAbilities( {
             gain( talent.berserk.enabled and 2 or 1 + ( allow_crit_prediction and crit_pct_current * active_enemies >= 230 and 1 or 0 ), "combo_points" )
 
             if talent.bloodtalons.enabled then
-                applyBuff( "bt_swipe_cat" )
+                applyBuff( "bt_swipe" )
                 check_bloodtalons()
             end
 
